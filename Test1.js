65fe55a7cf3f4754b56c532e3b46722b9eb2cd42
IT-Triphop <triphop.p@smartmedtech.co.th>
2023-02-09 10:56:52 +0700

    test for POS Developer

--- Test1.js
+++ Test1.js
@@ -1,3 +1,4 @@
+// ข้อที่ 2
 function intToChar(num) {
     var str = '';
     while (num > 0) {
@@ -7,15 +8,6 @@ function intToChar(num) {
     return str;
   }
   console.log(intToChar(703)); 
-  
-  
-  // sql test 1
-
-  SELECT name,totalSales, RANK() OVER (ORDER BY totalSales DESC) ranks FROM Sales;
-
-
-  // sql test 2
-// Select name, totalSales, Sum(totalSales) OVER (Order By totalSales desc) as Accumulate From sales Order By totalSales desc;
 
 
 
